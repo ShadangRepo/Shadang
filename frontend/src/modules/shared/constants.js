15da00a100835041afb4039325f90933195685a4
PurushottamBondre <purushottam.bondre66@gmail.com>
2021-08-28 14:33:36 +0530

    change base url for development

--- frontend/src/modules/shared/constants.js
+++ frontend/src/modules/shared/constants.js
@@ -1,6 +1,6 @@
 const Config = {
-    BASE_URL: "http://localhost:5001/shadang-63a81/us-central1/app"
-    // BASE_URL: process.env.NODE_ENV === "development" ? "https://us-central1-shadang-63a81.cloudfunctions.net/app" : "https://shadang.in"
+    // BASE_URL: "https://us-central1-shadang-63a81.cloudfunctions.net/app"
+    BASE_URL: process.env.NODE_ENV === "development" ? "http://localhost:5001/shadang-63a81/us-central1/app" : "https://shadang.in"
 }
 
 export { Config }
