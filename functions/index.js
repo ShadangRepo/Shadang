eaeca2f171788b39de9b6e9e0d0d324bab9aa982
PurushottamBondre <purushottam.bondre66@gmail.com>
2021-09-05 10:50:38 +0530

    enable cors

--- functions/index.js
+++ functions/index.js
@@ -4,21 +4,12 @@ const cors = require("cors");
 const app = express();
 const appRoutes = require("./apis")
 
-var whitelist = ['https://www.shadang.in', 'https://shadang.in']
-var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Origin blocked by CORS'))
-        }
-    }
-}
+app.use(cors())
 
-app.use(cors(corsOptions));
 app.use(express.urlencoded({
     extended: true
 }));
+
 app.use(express.json())
 
 app.use("/api", appRoutes);
